829c25aef32e22c76866d1fb6b5ef8927634973d
Hunter Gomulkiewicz <thehulksmasher109@gmail.com>
2026-03-02 16:48:51 -0600

    Added js code to simulate rolling a D8

--- Hitto209-and-GameDevHunterG's-Adventure/html/js/dice-roller.js
+++ Hitto209-and-GameDevHunterG's-Adventure/html/js/dice-roller.js
@@ -4,3 +4,8 @@ function rollD20(){
     rolledNumber = Math.floor(Math.random() * 20) + 1;
     document.getElementById('result').innerHTML = rolledNumber;
 }
+
+function rollD8(){
+    rolledNumber = Math.floor(Math.random() * 8) + 1; 
+    document.getElementById('result').innerHTML = rolledNumber;
+}
