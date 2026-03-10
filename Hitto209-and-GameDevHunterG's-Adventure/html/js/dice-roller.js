bad3e62365baf7f0424b68caa0fd812c5f97e1f0
Hunter Gomulkiewicz <thehulksmasher109@gmail.com>
2026-03-10 12:35:15 -0500

    added css to make dice roller show dice rolled

--- Hitto209-and-GameDevHunterG's-Adventure/html/js/dice-roller.js
+++ Hitto209-and-GameDevHunterG's-Adventure/html/js/dice-roller.js
@@ -2,10 +2,12 @@ let rolledNumber = 0;
 
 function rollD20(){
     rolledNumber = Math.floor(Math.random() * 20) + 1;
+    document.getElementById('dice').style.backgroundImage = 'url("/html/img/d20.png")';
     document.getElementById('result').innerHTML = rolledNumber;
 }
 
 function rollD8(){
     rolledNumber = Math.floor(Math.random() * 8) + 1; 
+    document.getElementById('dice').style.backgroundImage = 'url("/html/img/d8.jpg")';
     document.getElementById('result').innerHTML = rolledNumber;
 }
